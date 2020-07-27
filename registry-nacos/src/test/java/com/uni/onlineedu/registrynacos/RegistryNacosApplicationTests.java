54869ee8717e7c615e9f47df1ab06f95a34b6cbb
胡德钰 <hdy@unisolution.cn>
2020-07-27 17:45:44 +0800

    init

---
+++ registry-nacos/src/test/java/com/uni/onlineedu/registrynacos/RegistryNacosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uni.onlineedu.registrynacos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryNacosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
