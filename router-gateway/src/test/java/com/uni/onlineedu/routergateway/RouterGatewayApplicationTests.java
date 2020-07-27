e3011abd6cdd3f07de11e243fb3a37cf9f349444
胡德钰 <hdy@unisolution.cn>
2020-07-27 17:54:59 +0800

    add gateway

---
+++ router-gateway/src/test/java/com/uni/onlineedu/routergateway/RouterGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uni.onlineedu.routergateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouterGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
